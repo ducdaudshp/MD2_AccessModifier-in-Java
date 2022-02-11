06d287750d6672d8aec8156af14046c63a382fe1
ducdaudshp <ducdaudshp@gmail.com>
2022-02-11 15:28:49 +0700

    access modifier

--- Accessmodifier2/src/Student.java
+++ Accessmodifier2/src/Student.java
@@ -1,2 +1,24 @@
-package PACKAGE_NAME;public class Student {
+public class Student {
+    private String name="John";
+    private String classes="C02";
+    public Student(){
+
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getClasses() {
+        return classes;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setClasses(String classes) {
+        this.classes = classes;
+    }
+
 }
+
--- Accessmodifier2/src/Test.java
+++ Accessmodifier2/src/Test.java
@@ -1,2 +1,9 @@
-package PACKAGE_NAME;public class Test {
+public class Test {
+    public static void main(String[] args) {
+        Student s1=new Student();
+        s1.setName("Duc");
+        s1.setClasses("C12");
+        System.out.println(s1.getName());
+        System.out.println(s1.getClasses());
+    }
 }
